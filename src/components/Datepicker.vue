b42afb9d5b479c411998a4f850ae11645c34e945
Sebastian Martin <sebastianmartin@Sebastians-MacBook-Pro.local>
2021-01-11 09:52:10 +0100

    CalendarComponent; Calendar sectionMode

---
+++ src/components/Datepicker.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="datepicker component">
+    <!-- Calendar - Select Date-Range -->
+    <div class="date-range range-select">
+      <ul class="select-list">
+        <li class="select-item">
+          <RadioButton
+            v-model="selectionMode"
+            name="range"
+            value="single"
+            class="radio"
+            @click="setSelectionMode('single', $event)"
+          />
+          <p>Single</p>
+        </li>
+        <li class="select-item">
+          <RadioButton
+            v-model="selectionMode"
+            name="range"
+            value="range"
+            class="radio"
+            @click="setSelectionMode('range', $event)"
+          />
+          <p>Range</p>
+        </li>
+      </ul>
+    </div>
+
+    <!-- Calendar - Date-Picker -->
+    <div class="calendar-container">
+      <Calendar
+        ref="calendar"
+        v-model="date"
+        :inline="true"
+        :selection-mode="selectionMode"
+        class="calendar"
+        @date-select="setSelection($event)"
+      /> 
+    </div>
+    <div class="submit-container">
+      <Button 
+        class="submit-button"
+        @click="submitSelection"
+      >
+        Submit Selection
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Calendar from 'primevue/calendar';
+import Button from 'primevue/button';
+import RadioButton from 'primevue/radiobutton';
+
+export default {
+  name: 'Datepicker',
+  components: {
+    Calendar,
+    Button,
+    RadioButton,
+  },
+  data() {
+    return {
+      date: null,
+      selectionMode: 'single',
+      selecttion: [],
+    };
+  },
+  mounted() {
+    console.log(this.$refs.calendar);
+  },
+  methods: {
+    setSelectionMode(modeState, event) {
+      this.$refs.calendar.onClearButtonClick(event);
+      this.selectionMode = modeState;
+    },
+    setSelection(output) {
+      //convert Output
+      // let output = outputString.split(' ');
+      console.log({output});
+
+      switch(this.selectionMode) {
+        case 'single': 
+          break;
+        case 'range':
+          break;
+      }
+    },
+    getSelection(event) {
+    },
+    submitSelection(event) {
+      console.log('submit', event);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.date-range {
+  .select-list {
+    display: flex;
+    justify-content: center;
+    margin:0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  .select-item {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 8px;
+  }
+
+  .radio {
+    margin-right: 8px;
+  }
+}
+
+.calendar-container {
+  display: flex;
+  justify-content: center;
+}
+
+.submit-container {
+  display: flex;
+  justify-content: center;
+  padding: 8px;
+}
+</style>
+
+
--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -3,11 +3,49 @@
 </template>
 
 <script>
+import store from '@/store';
+import Datepicker from '@/components/Datepicker';
+import Button from 'primevue/button';
+
+
 export default {
-  name: 'Settings',
+  name: 'Stats',
+  components: {
+    Button,
+    Datepicker,
+  },
+  data() {
+    return {
+      date: null,
+    };
+  },
+  computed: {
+    recentReceipts() {
+      return store.getReceiptsSortedByDate().slice(-5).reverse();
+    },
+  },
+  mounted() {
+    console.log(this.recentReceipts);
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+// General-Styles @App.vue
+div {
+  padding: 15px;
+}
 
+.header {
+  margin: 0;
+  text-align: center;
+
+  h1 {
+    color: #2196F3;
+  }
+
+  h1, p {
+    margin: 0;
+  }
+}
 </style>
